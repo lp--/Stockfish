3d141bfefb38e9e8456f1b7a7b9eb55de78701d8
Leonid Pechenik <lp@home>
2013-08-28 17:34:12 -0400

    try take 7 only above 2 extra exchanges

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -395,7 +395,7 @@ Value do_evaluate(const Position& pos, Value& margin) {
       else
           // Endgame with opposite-colored bishops, but also other pieces. Still
           // a bit drawish, but not as drawish as with only the two bishops.
-	sf = ScaleFactor( std::min( 48, 40 +  popcount<Max15>(pos.pieces() ^ pos.pieces(PAWN)) )  );
+	sf = ScaleFactor( std::max( 50, 40 +  popcount<Max15>(pos.pieces() ^ pos.pieces(PAWN)) )  );
   }
 
   margin = margins[pos.side_to_move()];
