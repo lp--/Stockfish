51d427d0d9da5dbfe27f15765671dbbdb53432dc
Leonid Pechenik <lp@home>
2013-08-28 00:05:29 -0400

    scaling of take7  +  1 (41, 1)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -395,7 +395,7 @@ Value do_evaluate(const Position& pos, Value& margin) {
       else
           // Endgame with opposite-colored bishops, but also other pieces. Still
           // a bit drawish, but not as drawish as with only the two bishops.
-	sf = ScaleFactor( 39 +  popcount<Max15>(pos.pieces() ^ pos.pieces(PAWN))  );
+	sf = ScaleFactor( 41 +  popcount<Max15>(pos.pieces() ^ pos.pieces(PAWN))  );
   }
 
   margin = margins[pos.side_to_move()];
