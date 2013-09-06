98ff4f4ac818573a621e9a1f8a71284b88c5ebd8
Leonid Pechenik <lp@home>
2013-09-06 19:09:19 -0400

    46 when 3 pieces else 46

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -395,7 +395,7 @@ Value do_evaluate(const Position& pos, Value& margin) {
       else
           // Endgame with opposite-colored bishops, but also other pieces. Still
           // a bit drawish, but not as drawish as with only the two bishops.
-	sf = ScaleFactor( std::max( 50, 40 +  popcount<Max15>(pos.pieces() ^ pos.pieces(PAWN)) )  );
+	sf = ScaleFactor( popcount<Max15>(pos.pieces() ^ pos.pieces(PAWN)) < 7 ? 46 : 54   );
   }
 
   margin = margins[pos.side_to_move()];
