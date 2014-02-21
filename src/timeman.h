a1cffec4107d64a236b7ebb4aec74d8001d07cb1
Leonid Pechenik <lp@home>
2014-02-20 22:21:22 -0500

    slow mover 50

--- src/timeman.h
+++ src/timeman.h
@@ -27,7 +27,7 @@ class TimeManager {
 public:
   void init(const Search::LimitsType& limits, int currentPly, Color us);
   void pv_instability(double bestMoveChanges);
-  int available_time() const { return int(optimumSearchTime * unstablePvFactor * 0.83); }
+  int available_time() const { return int(optimumSearchTime * unstablePvFactor * 1.1); }
   int maximum_time() const { return maximumSearchTime; }
 
 private:
