800dde3ee37e7d92c662caa68a297dda77c785c2
Leonid Pechenik <lp@home>
2014-06-08 18:22:22 -0400

    shift by 1.5

--- src/search.cpp
+++ src/search.cpp
@@ -145,7 +145,10 @@ void Search::init() {
   for (d = 0; d < 32; ++d)
   {
       FutilityMoveCounts[0][d] = int(2.4 + 0.222 * pow(d + 0.00, 1.8));
-      FutilityMoveCounts[1][d] = int(3.0 + 0.300 * pow(d + 0.98, 1.8));
+      FutilityMoveCounts[1][d] = int(3.0 + 0.300 * pow(d + 1.5, 1.8));
+      //printf("%d\t%d\t%d\n", d, FutilityMoveCounts[0][d], FutilityMoveCounts[1][d] );
+
+
   }
 }
 
