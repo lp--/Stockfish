dddff98163f61d07bf4e4e9a17498eab996e6e25
Leonid Pechenik <lp@home>
2014-06-14 23:12:56 -0400

    lower to 16

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -161,7 +161,7 @@ namespace {
   const Score BishopPawns      = make_score( 8, 12);
   const Score MinorBehindPawn  = make_score(16,  0);
   const Score TrappedRook      = make_score(90,  0);
-  const Score Unstoppable      = make_score( 0, 24);
+  const Score Unstoppable      = make_score( 0, 16);
 
   // Penalty for a bishop on a1/h1 (a8/h8 for black) which is trapped by
   // a friendly pawn on b2/g2 (b7/g7 for black). This can obviously only
