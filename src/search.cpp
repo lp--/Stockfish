fb37a95f472d6f8cdc5214adc0f864679a7e3edb
Leonid Pechenik <lp@home>
2014-07-17 19:54:34 -0400

    95

--- src/search.cpp
+++ src/search.cpp
@@ -62,7 +62,7 @@ namespace {
   int FutilityMoveCounts[2][32]; // [improving][depth]
 
   inline Value futility_margin(Depth d) {
-    return Value(100 * d);
+    return Value(95 * d);
   }
 
   // Reduction lookup tables (initialized at startup) and their access function
